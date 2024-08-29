681121de7a8412d904a7259b104ad35d473b7f5f
priyadarshini-SF4715 <priyadarshini.d@syncfusion.com>
2024-08-29 19:30:02 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ProgressBarComponent as EjsProgressbar }
+ from '@syncfusion/ej2-vue-progressbar';
+</script>
+
+<template>
+<div class="container">
+  <ejs-progressbar height="160" width="160" isStriped="false"
+  minimum="1" maximum="50" value="25" trackThickness="12"
+  progressThickness="12" trackColor="#F8C7D8" progressColor="#E3165B"
+  type="Circular">
+  </ejs-progressbar>
+</div>
+</template>
+
+<style scoped>
+.container {
+  padding-bottom: 160px;
+}
+</style>
